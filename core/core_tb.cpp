7572322f623449a433f5693639dbf1e02a2cd022
LovisXII <logeofpit@yahoo.fr>
2023-03-15 17:08:02 +0100

    fixing null error on string

--- core/core_tb.cpp
+++ core/core_tb.cpp
@@ -21,7 +21,7 @@ using namespace ELFIO;
 
 #ifdef ICACHE_ON
 
-#include "CACHES/icache.h"ios::out
+#include "CACHES/icache.h"
 
 // ICACHE I/O INTERFACE MAE STATES
 enum IC_FSM
@@ -59,225 +59,265 @@ enum DC_FSM
 // arguments
 int sc_main(int argc, char* argv[]) {
     cout << "test" << endl;
-//     unordered_map<int, int> ram;
-//     elfio                   reader;  // creation of an elfio object
-//     string                  test_filename(argv[1]);
-//     string                  path(argv[1]);
-//     int                     reset_adr;
-//     int                     start_adr;
-//     int                     good_adr;
-//     int                     exception_occur ;
-//     int                     bad_adr;
-//     int                     rvtest_code_end;
-//     int                     rvtest_entry_point;
-//     int                     begin_signature;
-//     int                     end_signature;
-//     int                     rvtest_end;
+    unordered_map<int, int> ram;
+    elfio                   reader;  // creation of an elfio object
 
-//     fstream test_stats;
-//     string filename_stats;
+    if(argc < 2){
+        cerr << "First argument must be a .S, .s or .c file, please use --help" << endl;
+        exit(0);
+    }
+    string                  test_filename(argv[1]);
+    string                  path(argv[1]);
+    int                     reset_adr;
+    int                     start_adr;
+    int                     good_adr;
+    int                     exception_occur ;
+    int                     bad_adr;
+    int                     rvtest_code_end;
+    int                     rvtest_entry_point;
+    int                     begin_signature;
+    int                     end_signature;
+    int                     rvtest_end;
 
-//     char   test[512] = "> a.out.txt.s";
-//     string opt;
-//     string signature_name;
-//     bool   riscof;
-//     bool   stats;
-// /*
-//     ##############################################################
-//                     PARSING ELF/.s/.c file 
-//     ##############################################################
-// */
-//     if(argc == 2 && std::string(argv[1]) == "--help"){
-//         cout << endl << endl;
-//         cout << "Usage: ./core_tb test_filename [options] ..." << endl;
-//         cout << "Options:" << endl << endl;
-//         cout << "-O                          \t Optimise the .c file" << endl;
-//         cout << "--riscof signature_filename \t Allow to enable the riscof gestion and store the signature in the file named signature_filename" << endl ;
-//         cout << "--stats                     \t Allow to use the statistic such as the number of cycle needed to end the program" << endl;
-//         exit(0);
-//     }
-//     else if (argc >= 3 && std::string(argv[2]) == "-O") {
-//         opt = "-O2";
-//     } 
-//     else if (argc >= 3 && std::string(argv[2]) == "--riscof") {
-//         signature_name = string(argv[3]);
-//         riscof         = true;
-//         stats          = true;
+    fstream test_stats;
+    string filename_stats;
 
-//         int tmp = test_filename.find("src/");
-//         int tmp2 = test_filename.find("dut/");
+    char   test[512] = "> a.out.txt.s";
+    string opt;
+    string signature_name;
+    bool   riscof;
+    bool   stats;
+/*
+    ##############################################################
+                    PARSING ELF/.s/.c file 
+    ##############################################################
+*/
+    if(argc == 2 && std::string(argv[1]) == "--help"){
+        cout << endl << endl;
+        cout << "Usage: ./core_tb test_filename [options] ..." << endl;
+        cout << "Options:" << endl << endl;
+        cout << "-O                          \t Optimise the .c file" << endl;
+        cout << "--riscof signature_filename \t Allow to enable the riscof gestion and store the signature in the file named signature_filename" << endl ;
+        cout << "--stats                     \t Allow to use the statistic such as the number of cycle needed to end the program" << endl;
+        exit(0);
+    }
+    else if (argc >= 3 && std::string(argv[2]) == "-O") {
+        opt = "-O2";
+    } 
+    else if (argc >= 3 && std::string(argv[2]) == "--riscof") {
+        signature_name = string(argv[3]);
+        riscof         = true;
+        stats          = true;
 
-//         string tempo_string = test_filename.substr(0,tmp);
-//         string tempo_string2 = test_filename.substr(0,tmp2);
-//         int tmp3 = tempo_string2.size() - (tempo_string.size() + 4);
+        int tmp = test_filename.find("src/");
+        int tmp2 = test_filename.find("dut/");
 
-//         test_filename = test_filename.substr(tmp+4, tmp3);
+        string tempo_string = test_filename.substr(0,tmp);
+        string tempo_string2 = test_filename.substr(0,tmp2);
+        int tmp3 = tempo_string2.size() - (tempo_string.size() + 4);
+
+        test_filename = test_filename.substr(tmp+4, tmp3);
         
-//         #ifdef BRANCH_PREDICTION 
-//         filename_stats = "stats_branch_SS1.txt";        
-//         #elif  RET_BRANCH_PREDICTION
-//         filename_stats = "stats_stack_branch_SS1.txt";        
-//         #elif BRANCH_PREDICTION & RET_BRANCH_PREDICTION
-//         filename_stats = "stat_all_branch_SS1.txt";
-//         #elif ICACHE_ON & DCACHE_ON
-//         filename_stats = "stats_caches_SS1.txt";
-//         #else
-//         filename_stats = "test_stats_SS1.txt";
-//         #endif
-//         test_stats.open(filename_stats, fstream::app);
-//         if(!test_stats.is_open())
-//         {
-//             cout << "Impossible to open " << filename_stats << endl ;
-//             exit(1);
-//         }
-//     } 
-//     else if(argc >= 3 && std::string(argv[2]) == "--stats")
-//     {
-//         stats          = true;
-//         int tmp = test_filename.rfind("/");
-//         test_filename = test_filename.substr(tmp+1, test_filename.size());
+        #ifdef BRANCH_PREDICTION 
+        filename_stats = "stats_branch_SS1.txt";        
+        #elif  RET_BRANCH_PREDICTION
+        filename_stats = "stats_stack_branch_SS1.txt";        
+        #elif BRANCH_PREDICTION & RET_BRANCH_PREDICTION
+        filename_stats = "stat_all_branch_SS1.txt";
+        #elif ICACHE_ON & DCACHE_ON
+        filename_stats = "stats_caches_SS1.txt";
+        #else
+        filename_stats = "test_stats_SS1.txt";
+        #endif
+        test_stats.open(filename_stats, fstream::app);
+        if(!test_stats.is_open())
+        {
+            cout << "Impossible to open " << filename_stats << endl ;
+            exit(1);
+        }
+    } 
+    else if(argc >= 3 && std::string(argv[2]) == "--stats")
+    {
+        stats          = true;
+        int tmp = test_filename.rfind("/");
+        test_filename = test_filename.substr(tmp+1, test_filename.size());
         
-//         #ifdef BRANCH_PREDICTION 
-//         filename_stats = "stats_branch_SS1.txt";        
-//         #elif  RET_BRANCH_PREDICTION
-//         filename_stats = "stats_stack_branch_SS1.txt";        
-//         #elif BRANCH_PREDICTION & RET_BRANCH_PREDICTION
-//         filename_stats = "stat_all_branch_SS1.txt";
-//         #elif ICACHE_ON & DCACHE_ON
-//         filename_stats = "stats_caches_SS1.txt";
-//         #else
-//         filename_stats = "test_stats_SS1.txt";
-//         #endif
-//         test_stats.open(filename_stats, fstream::app);
-//         if(!test_stats.is_open())
-//         {
-//             cout << "Impossible to open " << filename_stats << endl ;
-//             exit(1);
-//         }
-//     }
+        #ifdef BRANCH_PREDICTION 
+        filename_stats = "stats_branch_SS1.txt";        
+        #elif  RET_BRANCH_PREDICTION
+        filename_stats = "stats_stack_branch_SS1.txt";        
+        #elif BRANCH_PREDICTION & RET_BRANCH_PREDICTION
+        filename_stats = "stat_all_branch_SS1.txt";
+        #elif ICACHE_ON & DCACHE_ON
+        filename_stats = "stats_caches_SS1.txt";
+        #else
+        filename_stats = "test_stats_SS1.txt";
+        #endif
+        test_stats.open(filename_stats, fstream::app);
+        if(!test_stats.is_open())
+        {
+            cout << "Impossible to open " << filename_stats << endl ;
+            exit(1);
+        }
+    }
 
-//     char temp_text[512];
-//     if (path.substr(path.find_last_of(".") + 1) == "s" || path.substr(path.find_last_of(".") + 1) == "S") {  
-//         char temp[512];
+    char temp_text[512];
+    if (path.substr(path.find_last_of(".") + 1) == "s" || path.substr(path.find_last_of(".") + 1) == "S") {  
+        char temp[512];
 
-//         sprintf(temp,
-//                 "riscv32-unknown-elf-gcc -nostdlib -march=rv32im -T ../../../SOFT/app.ld %s %s",
-//                 opt.c_str(),
-//                 path.c_str());  
+        sprintf(temp,
+                "riscv32-unknown-elf-gcc -nostdlib -march=rv32im -T ../../../SOFT/app.ld %s %s",
+                opt.c_str(),
+                path.c_str());  
                                 
-//         system((char*)temp);    
-//         path = "a.out";         
-//     }
-//     if (path.substr(path.find_last_of(".") + 1) == "c") { 
-//         char temp[512];
-//         sprintf(temp, "riscv32-unknown-elf-gcc -nostdlib -march=rv32im -T ../../../SOFT/app.ld %s %s",
-//          opt.c_str(),
-//          path.c_str());
-//         system((char*)temp);
-//         path = "a.out";
-//     }
-//     if (!reader.load(path)) {  
-//         std::cout << "Can't find or process ELF file " << argv[1] << std::endl;
-//         return -3;
-//     }
-//     sprintf(temp_text, "riscv32-unknown-elf-objdump -D %s", path.c_str());
-//     strcat(temp_text, test);
-//     system((char*)temp_text);
-//     cout << "Loading ELF file..." << endl;
+        system((char*)temp);    
+        path = "a.out";         
+    }
+    if (path.substr(path.find_last_of(".") + 1) == "c") { 
+        char temp[512];
+        sprintf(temp, "riscv32-unknown-elf-gcc -nostdlib -march=rv32im -T ../../../SOFT/app.ld %s %s",
+         opt.c_str(),
+         path.c_str());
+        system((char*)temp);
+        path = "a.out";
+    }
+    if (!reader.load(path)) {  
+        std::cout << "Can't find or process ELF file " << argv[1] << std::endl;
+        return -3;
+    }
+    sprintf(temp_text, "riscv32-unknown-elf-objdump -D %s", path.c_str());
+    strcat(temp_text, test);
+    system((char*)temp_text);
+    cout << "Loading ELF file..." << endl;
  
-//     int n_sec = reader.sections.size();  // get the total amount of sections
+    int n_sec = reader.sections.size();  // get the total amount of sections
 
-// /*
-//     ##############################################################
-//                     PLACING DATA INTO THE RAM 
-//     ##############################################################
-// */
+/*
+    ##############################################################
+                    PLACING DATA INTO THE RAM 
+    ##############################################################
+*/
 
-//     for (int i = 0; i < n_sec; i++) {
-//         section* sec = reader.sections[i];
-//         cout << "Section " << sec->get_name() << " at address 0x" << std::hex << sec->get_address() << endl;
-//         int  adr  = sec->get_address();
-//         int  size = sec->get_size();
-//         int* data = (int*)sec->get_data();
-//         if (adr) {
-//             cout << "Loading data";
-//             for (int j = 0; j < size; j += 4) {
-//                 cout << ".";
-//                 ram[adr + j] = data[j / 4]; 
-//             }
-//             cout << endl;
-//         }
+    for (int i = 0; i < n_sec; i++) {
+        section* sec = reader.sections[i];
+        cout << "Section " << sec->get_name() << " at address 0x" << std::hex << sec->get_address() << endl;
+        int  adr  = sec->get_address();
+        int  size = sec->get_size();
+        int* data = (int*)sec->get_data();
+        if (adr) {
+            cout << "Loading data";
+            for (int j = 0; j < size; j += 4) {
+                cout << ".";
+                ram[adr + j] = data[j / 4]; 
+            }
+            cout << endl;
+        }
 
-// /*
-//     ##############################################################
-//                     LOOKING FOR SECTIONS IN ELF FILE 
-//     ##############################################################
-// */
+/*
+    ##############################################################
+                    LOOKING FOR SECTIONS IN ELF FILE 
+    ##############################################################
+*/
 
-//         if (sec->get_type() == SHT_SYMTAB) {
-//             cout << "Reading symbols table..." << endl;
-//             const symbol_section_accessor symbols(reader, sec);
-//             for (unsigned int j = 0; j < symbols.get_symbols_num(); ++j) {
-//                 std::string   name;
-//                 Elf64_Addr    value;
-//                 Elf_Xword     size;
-//                 unsigned char bind;
-//                 unsigned char type;
-//                 Elf_Half      section_index;
-//                 unsigned char other;
+        if (sec->get_type() == SHT_SYMTAB) {
+            cout << "Reading symbols table..." << endl;
+            const symbol_section_accessor symbols(reader, sec);
+            for (unsigned int j = 0; j < symbols.get_symbols_num(); ++j) {
+                std::string   name;
+                Elf64_Addr    value;
+                Elf_Xword     size;
+                unsigned char bind;
+                unsigned char type;
+                Elf_Half      section_index;
+                unsigned char other;
 
-//                 symbols.get_symbol(j, name, value, size, bind, type, section_index, other);
-//                 if (name == "_reset") {
-//                     cout << "Found reset" << endl;
-//                     reset_adr = value - 4;  // minus 4 to acount for init inc_pc
-//                 }
-//                 if (name == "_start") {
-//                     cout << "Found start" << endl;
-//                     start_adr = value - 4;  // minus 4 to acount for init inc_pc
-//                 }
-//                 if (name == "_bad") {
-//                     cout << "Found bad" << endl;
-//                     bad_adr = value;
-//                 }
-//                 if (name == "_good") {
-//                     cout << "Found good" << endl;
-//                     good_adr = value;
-//                 }
-//                 if (name == "_exception_occur") {
-//                     cout << "Found exception_occur" << endl;
-//                     exception_occur = value;
-//                 }
-//                 if (name == "rvtest_code_end") {
-//                     cout << "Found rvtest_code_end" << endl;
-//                     rvtest_code_end = value;
-//                 }
-//                 if (name == "rvtest_entry_point") {
-//                     cout << "Found rvtest_entry_point" << endl;
-//                     reset_adr = value - 4;
-//                 }
-//                 if (name == "begin_signature") {
-//                     cout << "Found begin_signature" << endl;
-//                     begin_signature = value;
-//                 }
-//                 if (name == "end_signature") {
-//                     cout << "Found end_signature" << endl;
-//                     end_signature = value;
-//                 }
-//                 if (name == "rvtest_end") {
-//                     rvtest_end = value;
-//                     cout << "Found rvtest_end at adr " << std::hex << rvtest_end << endl;
-//                 }
-//             }
-//         }
-//     }
+                symbols.get_symbol(j, name, value, size, bind, type, section_index, other);
+                if (name == "_reset") {
+                    cout << "Found reset" << endl;
+                    reset_adr = value - 4;  // minus 4 to acount for init inc_pc
+                }
+                if (name == "_start") {
+                    cout << "Found start" << endl;
+                    start_adr = value - 4;  // minus 4 to acount for init inc_pc
+                }
+                if (name == "_bad") {
+                    cout << "Found bad" << endl;
+                    bad_adr = value;
+                }
+                if (name == "_good") {
+                    cout << "Found good" << endl;
+                    good_adr = value;
+                }
+                if (name == "_exception_occur") {
+                    cout << "Found exception_occur" << endl;
+                    exception_occur = value;
+                }
+                if (name == "rvtest_code_end") {
+                    cout << "Found rvtest_code_end" << endl;
+                    rvtest_code_end = value;
+                }
+                if (name == "rvtest_entry_point") {
+                    cout << "Found rvtest_entry_point" << endl;
+                    reset_adr = value - 4;
+                }
+                if (name == "begin_signature") {
+                    cout << "Found begin_signature" << endl;
+                    begin_signature = value;
+                }
+                if (name == "end_signature") {
+                    cout << "Found end_signature" << endl;
+                    end_signature = value;
+                }
+                if (name == "rvtest_end") {
+                    rvtest_end = value;
+                    cout << "Found rvtest_end at adr " << std::hex << rvtest_end << endl;
+                }
+            }
+        }
+    }
 
+    cout << "test2" << endl;
 /*
     ##############################################################
                     COMPONENT INSTANCIATION
     ##############################################################
 */
 
-    // Vcore core_inst("core_inst");
+    Vcore core_inst("core_inst");
+
+
+    sc_signal<bool> clk;
+    sc_signal<bool> reset_n;
+    sc_signal<bool> MCACHE_STALL_SM;
+    sc_signal<bool> MCACHE_ADR_VALID_SM;
+    sc_signal<bool> MCACHE_STORE_SM;
+    sc_signal<bool> MCACHE_LOAD_SM;
+    sc_signal<uint32_t> byt_sel;
+    sc_signal<bool> IC_STALL_SI;
+    sc_signal<bool> ADR_VALID_SI;
+    sc_signal<uint32_t> MCACHE_RESULT_SM;
+    sc_signal<uint32_t> MCACHE_DATA_SM;
+    sc_signal<uint32_t> MCACHE_ADR_SM;
+    sc_signal<uint32_t> IC_INST_SI;
+    sc_signal<uint32_t> ADR_SI;
+    sc_signal<uint32_t> PC_INIT;
+    sc_signal<uint32_t> DEBUG_PC_READ;
+
+    core_inst.clk(clk);
+    core_inst.reset_n(reset_n);
+    core_inst.MCACHE_STALL_SM(MCACHE_STALL_SM);
+    core_inst.MCACHE_ADR_VALID_SM(MCACHE_ADR_VALID_SM);
+    core_inst.MCACHE_STORE_SM(MCACHE_STORE_SM);
+    core_inst.MCACHE_LOAD_SM(MCACHE_LOAD_SM);
+    core_inst.byt_sel(byt_sel);
+    core_inst.IC_STALL_SI(IC_STALL_SI);
+    core_inst.ADR_VALID_SI(ADR_VALID_SI);
+    core_inst.MCACHE_RESULT_SM(MCACHE_RESULT_SM);
+    core_inst.MCACHE_DATA_SM(MCACHE_DATA_SM);
+    core_inst.IC_INST_SI(IC_INST_SI);
+    core_inst.ADR_SI(ADR_SI);
+    core_inst.PC_INIT(PC_INIT);
+    core_inst.DEBUG_PC_READ(DEBUG_PC_READ);
 
 // #ifdef ICACHE_ON
 //     icache icache_inst("icache");
--- core/obj_dir/Vcore.mk
+++ core/obj_dir/Vcore.mk
@@ -12,9 +12,9 @@ PERL = perl
 # Path to Verilator kit (from $VERILATOR_ROOT)
 VERILATOR_ROOT = /usr/share/verilator
 # SystemC include directory with systemc.h (from $SYSTEMC_INCLUDE)
-SYSTEMC_INCLUDE ?= /usr/include
+SYSTEMC_INCLUDE ?= /usr/local/systemc-2.3.3/include/
 # SystemC library directory with libsystemc.a (from $SYSTEMC_LIBDIR)
-SYSTEMC_LIBDIR ?= /usr/lib/x86_64-linux-gnu
+SYSTEMC_LIBDIR ?= /usr/local/systemc-2.3.3/lib-linux64/
 
 ### Switches...
 # SystemC output mode?  0/1 (from --sc)
@@ -33,7 +33,7 @@ VM_PREFIX = Vcore
 VM_MODPREFIX = Vcore
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-I../ELFIO  -Wl, -lsystemc -lm -g  \
+	-I../../ELFIO  -Wl, -lsystemc -lm -g  \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
